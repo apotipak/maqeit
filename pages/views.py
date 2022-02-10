79900c69c04dbcf5bd388c22146fcb166f70836b
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2022-02-11 04:10:58 +0700

    Add initial files.

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# @login_required(login_url='/accounts/login/')
+def index(request):    
+    
+    page_title = "HOME"
+    
+    return render(request, 'index.html', {
+        'page_title': page_title, 
+    })
